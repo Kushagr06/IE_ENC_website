4385880a8daedf2100eef4fe5e5bcf6772e85d8c
Kushagr Narayan <nkushagr@gmail.com>
2023-06-09 10:58:45 +0530

    fr2_1

--- src/components/WhoWeAre.js
+++ src/components/WhoWeAre.js
@@ -3,9 +3,11 @@ import Heading from './Heading';
 
 function WhoWeAre() {
   return (
+    <>
     <div className='mx-10'>
       <Heading text="Who We Are" subtext=" We are the official Electroniczs and Communication sclub of Manipal.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni modi repellat illum deleniti consectetur ex obcaecati ipsum repudiandae commodi molestiae sapiente aliquam praesentium fugiat impedit vitae, enim minus eveniet accusantium." /> 
     </div>
+    </>
   )
 }
 
