a7a11236ffd7dd22d0f8b2cae849448d7dc369e0
Kushagr Narayan <nkushagr@gmail.com>
2023-07-07 00:15:44 +0530

    events_edited

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   return (
     <div className='bg-background text-textcol overflow-hidden' id="main">
       <Navbar />
+      
       <Hero />
       <WhoWeAre />
       <motion.div
