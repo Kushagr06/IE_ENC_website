72407a5a2562b1c4844ff55ab9a7ac764ef2fc63
Kushagr Narayan <nkushagr@gmail.com>
2023-08-14 00:43:41 +0530

    fr_ss

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -42,11 +42,11 @@ export default class Resources extends Component {
         > 
       {/* Stamp */}
     <div className='flex justify-center mt-12'>
-      <div className='w-[75%] m-10 p-20 h-auto bg-primary rounded-2xl border-4 border-secondary align-middle flex justify-around text-[2.5vh] text-secondary md:flex-row flex-col font-medium relative overflow-clip'>
-        <img src={Flower} alt="" id="stamp"  className='absolute -top-[16vh] -left-[16vh] sm:scale-100 scale-[.68]'/>
-        <img src={Globe} alt="" id="stamp"  className='absolute -bottom-[16vh] -right-[16vh] sm:scale-100 scale-[.68]'/>
+      <div className='sm:w-[60%] w-[75%] m-10 p-20 h-auto bg-primary rounded-2xl border-4 border-secondary align-middle flex justify-around text-[2.5vh] text-secondary md:flex-row flex-col font-medium relative overflow-clip'>
+        <img src={Flower} alt="" id="stamp"  className='absolute -top-[16vh] -left-[16vh] sm:scale-100 scale-[.65]'/>
+        <img src={Globe} alt="" id="stamp"  className='absolute -bottom-[16vh] -right-[16vh] sm:scale-100 scale-[.65]'/>
           
-          <div className='  my-2'>
+          <div className=' flex flex-col'>
             <div className='text-[2.5vh] font-medium rounded-2xl p-1 border-2 border-secondary  text-center'>
             Founded
             </div>
@@ -58,7 +58,7 @@ export default class Resources extends Component {
         
 
 
-          <div className='  my-2'>
+          <div className='flex flex-col '>
             <div className='text-[2.5vh] font-medium rounded-2xl p-1 border-2 border-secondary  text-center'>
             Location
             </div>
