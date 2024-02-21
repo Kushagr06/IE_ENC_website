f472e5fd281ffb9cd0cbc3383195094228faa653
anannyashukla <115167160+anannyashukla@users.noreply.github.com>
2024-02-21 21:24:43 +0530

    Update Upcoming.js

--- src/components/Upcoming.js
+++ src/components/Upcoming.js
@@ -10,7 +10,9 @@ function Upcoming() {
       <div className='mx-5 mb-16'>
     //<a href="https://ibb.co/kD0yPRk"> <img src="https://imgur.com/7Lo2kvK"> 
    // <a href="https://ibb.co/0yZgNC7">
-    <img src="https://i.ibb.co/whC9HSk/Whats-App-Image-2024-02-21-at-7-08-46-PM.jpg" alt="Whats-App-Image-2024-02-21-at-7-08-46-PM" border="0">
+    
+   // <img src= "https://imgur.com/7Lo2kvK" />
+    <img src="https://i.ibb.co/whC9HSk/Whats-App-Image-2024-02-21-at-7-08-46-PM.jpg" alt="Whats-App-Image-2024-02-21-at-7-08-46-PM" border="0"/>
     //</a>
       <Heading text="Upcoming" subtext="Lookut! Here are the Upcoming Events for the season." /> 
     </div>
