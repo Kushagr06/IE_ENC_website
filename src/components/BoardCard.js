e75dff68e1d24bc4deb8777a396b51ff6fa42ca6
anannyashukla <115167160+anannyashukla@users.noreply.github.com>
2024-02-21 13:28:48 +0530

    Update BoardCard.js

--- src/components/BoardCard.js
+++ src/components/BoardCard.js
@@ -25,7 +25,7 @@ function BoardCard() {
        
          {/* picture */}
          <div className='rounded-full h-[7.5rem] w-[7.5rem] bg-primary  border-2 border-primary -mr-16 z-10 overflow-hidden object-center relative shadow-md shadow-background hover:border-highlight'>
-            <a href="https://drive.google.com/file/d/1GJavXOX4ir6XUQVBrZJffd5kCRG9iPbG/view"><img src="https://drive.google.com/file/d/1GJavXOX4ir6XUQVBrZJffd5kCRG9iPbG/view" alt="Jeewant" border="0"></a>
+            <a href="Jeewant.jpg"><img src="Jeewant.jpg" alt="Jeewant" border="0"></a>
     //<img src={`https://drive.google.com/uc?export=view&id=`+info.pic} alt=" " className='w-auto bg-cover max-w-full absolute top-2 left-0 h-auto scale-[1.15]'/>
   //<img src= {`https://drive.usercontent.google.com/download?id=`+info.pic + &export=view&authuser=0}  alt=" " className='w-auto bg-cover max-w-full absolute top-2 left-0 h-auto scale-[1.15]'/>
           </div>
